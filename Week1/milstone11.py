73ca21ef0626597f598a030d3ef0a2da2cafcacf
SandraDGomez <sandra.dominguez.gomez20@gmail.com>
2023-09-23 20:55:29 +0200

    Comments

--- Week1/milstone11.py
+++ Week1/milstone11.py
@@ -14,8 +14,8 @@ U= array([1, 0, 0, 1])
 print( type (U))
 
 dt =0.01
-x = array( zeros(N) )
-y = array( zeros(N) )
+x = zeros(N)
+y = zeros(N) 
 x[0]=U[0]
 y[0]=U[0]
 
@@ -27,6 +27,12 @@ for i in range (0, N):
     U = U + dt * F
     x[i]=U[0]
     y[i]=U[1]
+    
+    # U = zeros( (4, N) )
+    # U_0 = [1,0,0,1] 
+    # U[:,0] = U_0 signfica que todas las filas (:) la primera columna, que en python empieza por 0, tomen de valor ese vector.
+    # for i in range(N):
+    #   U[:, i+1] = U[:,i] + dt * F_kepler(U[:,i])
 
 
 plt.plot ( x, y )
